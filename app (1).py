391d7caa2319932e9ab2e7640f05c56f19e74c9d
Tijesunimi Olashore <tijesunimiolashore@gmail.com>
2026-05-14 15:21:51 +0100

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -7,7 +7,7 @@ from prophet import Prophet
 st.title("Disease Forecast Dashboard")
 
 # Load dataset
-df = pd.read_csv("dataset for workshop.csv")
+df = pd.read_csv("dataset for workshop.xls")
 
 st.sidebar.header("Forecast Settings")
 
